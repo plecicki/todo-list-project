778002bce42481c9dd63e04ae1cedc68fb3b4d1b
Piotr Lecicki <p.lecicki@gmail.com>
2025-06-20 19:39:54 +0200

    Implement server

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
+});
+
+const tasks = [];
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', (task) => {
+    const tasksArrayIndex = tasks.findIndex((i) => {
+      return i.id === task.id;
+    })
+    tasks.splice(tasksArrayIndex, 1);
+    socket.broadcast.emit('removeTask', task);
+  });
 });
 
 app.use((req, res) => {
