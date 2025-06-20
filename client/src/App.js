15f926c56378ca4827e34a5df93fae488486b359
Piotr Lecicki <p.lecicki@gmail.com>
2025-06-20 19:26:15 +0200

    Add base client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+        <h1>ToDoList.app</h1>
       </header>
+
+      <section className="tasks-section" id="tasks-section">
+        <h2>Tasks</h2>
+
+        <ul className="tasks-section__list" id="tasks-list">
+          <li className="task">Shopping <button className="btn btn--red">Remove</button></li>
+          <li className="task">Go out with a dog <button className="btn btn--red">Remove</button></li>
+        </ul>
+
+        <form id="add-task-form">
+          <input className="text-input" autoComplete="off" type="text" placeholder="Type your description"
+                 id="task-name"/>
+          <button className="btn" type="submit">Add</button>
+        </form>
+
+      </section>
     </div>
   );
 }
